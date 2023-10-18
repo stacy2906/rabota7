352c339a52e5a92ec3da0117ace577705c7dec23
stacy2906 <anastasiya.tkachyk.290606@gmail.com>
2023-10-18 21:40:24 +0300

    add program.cs

---
+++ ConsoleApp15/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Дано название футбольного клуба.Определить количество символов в нем.
+
+            //Console.Write("Введите название футбольного клуба: ");
+            //string clubName = Console.ReadLine();
+
+            //int characterCount = clubName.Length;
+
+            //Console.WriteLine($"Количество символов в названии клуба: {characterCount}");
+
+            //Задача 2.Дано название города. Определить, четно или нет количество символов в нем.
+
+            //Console.Write("Введите название города: ");
+            //string cityName = Console.ReadLine();
+
+            //int characterCount = cityName.Length;
+
+            //if (characterCount % 2 == 0)
+            //{
+            //    Console.WriteLine("Количество символов в названии города четное.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Количество символов в названии города нечетное.");
+            //}
+
+            //Задача 3.Даны две фамилии. Определить, какая из них длиннее.
+
+            //Console.Write("Введите первую фамилию: ");
+            //string lastName1 = Console.ReadLine();
+
+            //Console.Write("Введите вторую фамилию: ");
+            //string lastName2 = Console.ReadLine();
+
+            //int length1 = lastName1.Length;
+            //int length2 = lastName2.Length;
+
+            //if (length1 > length2)
+            //{
+            //    Console.WriteLine($"Первая фамилия ({lastName1}) длиннее второй фамилии ({lastName2}).");
+            //}
+            //else if (length2 > length1)
+            //{
+            //    Console.WriteLine($"Вторая фамилия ({lastName2}) длиннее первой фамилии ({lastName1}).");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Обе фамилии одинаковой длины.");
+            //}
+
+            //Задача 4.Найти самое длинное слово из пяти введенных с клавиатуры.
+
+            //    Console.WriteLine("Введите пять слов:");
+
+            //    string[] words = new string[5];
+
+            //    for (int i = 0; i < 5; i++)
+            //    {
+            //        Console.Write($"Слово {i + 1}: ");
+            //        words[i] = Console.ReadLine();
+            //    }
+
+            //    string longestWord = FindLongestWord(words);
+
+            //    Console.WriteLine($"Самое длинное слово: {longestWord}");
+            //}
+
+            //static string FindLongestWord(string[] words)
+            //{
+            //    string longest = "";
+            //    foreach (string word in words)
+            //    {
+            //        if (word.Length > longest.Length)
+            //        {
+            //            longest = word;
+            //        }
+            //    }
+            //    return longest;
+
+            //Задача 5.Составить программу, которая запрашивает отдельно имя и отдельно фамилию, а затем выводит их как одну символьную строку.
+
+            //Console.Write("Введите имя: ");
+            //string firstName = Console.ReadLine();
+
+            //Console.Write("Введите фамилию: ");
+            //string lastName = Console.ReadLine();
+
+            //string fullName = firstName + " " + lastName;
+
+            //Console.WriteLine("Полное имя: " + fullName);
+
+            //Задача 6.Подсчитать количество букв "а"("А") в строке.
+
+            //    Console.Write("Введите строку: ");
+            //    string inputString = Console.ReadLine();
+
+            //    int count = CountLetterA(inputString);
+
+            //    Console.WriteLine($"Количество букв 'а' или 'А' в строке: {count}");
+            //}
+
+            //static int CountLetterA(string input)
+            //{
+            //    int count = 0;
+            //    foreach (char c in input)
+            //    {
+            //        if (c == 'a' || c == 'A')
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    return count;
+
+            //Задача 7.Подсчитать количество слов в предложении, при условии, что каждое слово отделяется от другого пробелом.
+
+        //    Console.Write("Введите предложение: ");
+        //    string sentence = Console.ReadLine();
+
+        //    int wordCount = CountWords(sentence);
+
+        //    Console.WriteLine($"Количество слов в предложении: {wordCount}");
+        //}
+
+        //static int CountWords(string input)
+        //{
+            
+        //    string[] words = input.Split(' ');
+
+        //    return words.Length;
+
+
+
+        }
+    }
+}
